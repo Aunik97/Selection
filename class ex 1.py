73ea8754811c82631513aae6fbd0c61ab2ea813c
Aunik Hussain <31704@longroad.ac.uk>
2014-09-30 15:06:50 +0100

    class ex

---
+++ class ex 1.py
@@ -0,0 +1,10 @@
+#Aunik Hussain
+#Selection revision ex 1
+#29-09-2014
+
+first_number = int(input("Please enter your first number"))
+second_number = int(input("Please enter your second number"))
+
+if first_number == second_number:
+    print("These two numbers are equal")
+
