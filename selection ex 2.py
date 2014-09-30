d83474556576aef1c37e559c501969bd432cd3b4
Aunik Hussain <31704@longroad.ac.uk>
2014-09-30 10:15:20 +0100

    commit

---
+++ selection ex 2.py
@@ -0,0 +1,11 @@
+#Aunik Hussain
+#30-09-2014
+#Selection ex 2
+
+age = int(input("please enter your age"))
+
+if age > 17:
+    print("you are old enough to drive")
+else:
+    print("sorry you are too young to drive")
+        
